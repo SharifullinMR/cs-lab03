9da957c5213ff25fe05fd66eeac4a5a30a715f77
SharifullinMR <SharifullinMR@mpei.ru>
2022-04-11 23:21:42 +0300

    Выделение в модуль SVG функций SVG

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "histogram.h"
+#include "SVG.h"
 using namespace std;
 vector<double> input_numbers(size_t count){
     vector<double> result(count);
@@ -35,84 +36,6 @@ vector<size_t> make_histogram(const vector<double> &numbers, size_t bin_count)
         }
     }
     return bins;
-}
-void show_histogram_text(const vector<size_t> &bins){
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1 ;
-    size_t max_bin = bins[0];
-    for (size_t bin : bins){
-        if (bin>max_bin){
-            max_bin=bin;
-        }
-    }
-    for (size_t bin: bins)
-    {
-        double height = bin;
-        if (max_bin > MAX_ASTERISK){
-            height = MAX_ASTERISK * (static_cast<double>(bin)/max_bin);
-        }
-        if (bin < 100){
-            cout<<" ";
-        }
-        if (bin < 10){
-            cout<<" ";
-        }
-        cout<<bin<<"|";
-        for (size_t i = 0; i<height; i++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-}
-void svg_begin(double width, double height) {
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
-void svg_end(){
-    cout << "</svg>\n";
-}
-void svg_text(double left, double baseline, string text){
-    cout<<"<text x='"<<left<<"' y='"<<baseline<<"'>"<<text<<"</text>";
-}
-void svg_rect(double x, double y, double width, double height,string stroke="#990000", string fill = "#66FFFF"){
-    cout<<"<rect x='"<<x<<"' y='"<<y<<"' width='"<<width<<"' height='"<<height<<"' stroke='"<<stroke<<"' fill='"<<fill<< "' />";
-}
-void show_histogram_svg(const vector<size_t>& bins) {
-    {
-        const auto IMAGE_WIDTH = 400;
-        const auto IMAGE_HEIGHT = 300;
-        const auto TEXT_LEFT = 20;
-        const auto TEXT_BASELINE = 20;
-        const auto TEXT_WIDTH = 50;
-        const auto BIN_HEIGHT = 30;
-        const auto BLOCK_WIDTH = 10;
-        svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-        double top = 0;
-        const size_t SCREEN_WIDTH = 80;
-        const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1 ;
-        size_t max_bin = bins[0];
-        for (size_t bin : bins){
-            if (bin>max_bin){
-                max_bin=bin;
-            }
-        }
-        for (size_t bin: bins)
-        {
-            double height = bin;
-            if (max_bin > MAX_ASTERISK){
-                height = MAX_ASTERISK * (static_cast<double>(bin)/max_bin);
-            }
-            const double bin_width = BLOCK_WIDTH * height;
-            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-            svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
-            top += BIN_HEIGHT;
-        }
-        svg_end();
-    }
 }
 int main()
 {
