26bc99f5b79bbdbb468b77b2fbed5aa7f64c88b5
SharifullinMR <SharifullinMR@mpei.ru>
2022-05-06 15:06:13 +0300

    make_histogram + Input

--- histogram.h
+++ histogram.h
@@ -3,5 +3,8 @@
 #include <vector>
 using namespace std;
 void find_minmax(const vector<double> &numbers, double &min, double &max);
-
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
 #endif // HISTOGRAM_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@ vector<double> input_numbers(istream& in,size_t count){
     }
     return result;
 }
-struct Input{
-    vector<double> numbers;
-    size_t bin_count;
-};
 Input read_input(istream& in){
     Input data;
     size_t number_count;
@@ -26,16 +22,16 @@ Input read_input(istream& in){
     in>>data.bin_count;
     return data;
 }
-vector<size_t> make_histogram(const vector<double> &numbers, size_t bin_count)
+vector<size_t> make_histogram(Input input)
 {
     double min,max;
-    find_minmax(numbers, min , max);
-    double bin_size = (max-min)/bin_count;
-    vector <size_t> bins(bin_count, 0);
-    for (double number_count : numbers)
+    find_minmax(input.numbers, min , max);
+    double bin_size = (max-min)/input.bin_count;
+    vector <size_t> bins(input.bin_count, 0);
+    for (double number_count : input.numbers)
     {
         bool found = false;
-        for(size_t j = 0; j < (bin_count-1) && !found; j++)
+        for(size_t j = 0; j < (input.bin_count-1) && !found; j++)
         {
             auto lo = min + j * bin_size;
             auto hi = min + ( j + 1 ) * bin_size;
@@ -47,7 +43,7 @@ vector<size_t> make_histogram(const vector<double> &numbers, size_t bin_count)
         }
         if (!found)
         {
-            bins[bin_count-1]++;
+            bins[input.bin_count-1]++;
         }
     }
     return bins;
@@ -55,7 +51,7 @@ vector<size_t> make_histogram(const vector<double> &numbers, size_t bin_count)
 int main()
 {
     const auto input = read_input(cin);
-    const auto bins = make_histogram(input.numbers,input.bin_count);
+    const auto bins = make_histogram(input);
     show_histogram_svg(bins);
     return 0;
 }
