a31761406173a5dbe4cdda31f38bcea68631662b
SharifullinMR <SharifullinMR@mpei.ru>
2022-05-06 13:20:10 +0300

    Ф-ия input_numbers

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 #include "histogram.h"
 #include "SVG.h"
 using namespace std;
-vector<double> input_numbers(size_t count){
+vector<double> input_numbers(istream& in,size_t count){
     vector<double> result(count);
     for (size_t i = 0; i<count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -43,7 +43,7 @@ int main()
     cerr<<"Enter number count:";
     cin >> number_count;
     cerr<<"Enter numbers:";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin,number_count);
     size_t bin_count;
     cerr <<"Enter bin count:";
     cin >> bin_count;
