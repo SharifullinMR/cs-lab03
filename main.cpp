e82c9d15b876b31d9c8ae49e862295cc3a36548d
SharifullinMR <SharifullinMR@mpei.ru>
2022-05-09 21:46:19 +0300

    загрузка файла по сети

--- main.cpp
+++ main.cpp
@@ -58,9 +58,12 @@ vector<size_t> make_histogram(Input input)
 int main(int argc,char* argv[])
 {
     if (argc>1){
-        cout<<"argc = "<<argc<<endl;
-        for(size_t i = 0; i<argc; i ++ ){
-            cout<<"argv["<<i<<"] = "<<argv[i]<<endl;
+        CURL* curl = curl_easy_init();
+        if(curl){
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
         }
         return 0;
     }
