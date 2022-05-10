2b992596fe42c253a3723b76cc1e9303b98fb5cf
SharifullinMR <SharifullinMR@mpei.ru>
2022-05-10 09:58:16 +0300

    работа и сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
 #include "histogram.h"
 #include "SVG.h"
@@ -55,24 +57,41 @@ vector<size_t> make_histogram(Input input)
     }
     return bins;
 }
-int main(int argc,char* argv[])
-{
-    if (argc>1){
-        CURL* curl = curl_easy_init();
-        if(curl){
-            CURLcode res;
-            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-            if (!CURLE_OK){
-                cerr<<" Error text  = "<<curl_easy_strerror(res);
-                exit(1);
-            }
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    size_t data_size = item_size * item_count;
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    buffer->write(reinterpret_cast<const char*>(items), data_size);
+    return data_size;
+}
+Input
+download(const string& address) {
+    stringstream buffer;
+    CURL* curl = curl_easy_init();
+    if(curl){
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        if (!CURLE_OK){
+            cerr<<" Error text  = "<<curl_easy_strerror(res);
+            exit(1);
         }
-        return 0;
     }
     curl_global_init(CURL_GLOBAL_ALL);
-    const auto input = read_input(cin,true);
+    return read_input(buffer, false);
+}
+int main(int argc,char* argv[])
+{
+    Input input;
+    if (argc>1){
+        input = download(argv[1]);
+    }
+    else{
+        input = read_input(cin,true);
+    }
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
     return 0;
