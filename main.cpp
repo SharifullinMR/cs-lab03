0ad471ed7bd028e0f6bbfaf1a42f04086f78aac3
SharifullinMR <SharifullinMR@mpei.ru>
2022-05-25 20:26:27 +0300

    форматный вывод

--- main.cpp
+++ main.cpp
@@ -39,6 +39,11 @@ vector<size_t> make_histogram(const vector<double> &numbers, size_t bin_count)
 }
 int main()
 {
+    const char* name = "Commander Mayson";
+    int years = 3500;
+    printf("%s was born in %d.\n", name, years);
+    printf("n = %08x\n", 0x12343457);
+    return 0;
     size_t number_count;
     cerr<<"Enter number count:";
     cin >> number_count;
